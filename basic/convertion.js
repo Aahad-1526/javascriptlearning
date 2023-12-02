698bd8c16f52ca93af4ffda5b7803587d92bc416
Aahad-1526 <128283478+Aahad-1526@users.noreply.github.com>
2023-12-02 09:16:35 +0000

    convertion understanding

---
+++ basic/convertion.js
@@ -0,0 +1,37 @@
+let age = 33
+console.log(typeof age);
+
+let ageString = String (age)
+console.log(ageString);
+console.log(typeof ageString);
+
+
+let nameString = "aahad"
+let  newString = Number(nameString)
+
+console.log(typeof newString );
+console.log(newString);
+
+let logIn = undefined
+let newLogIn = Boolean (logIn)
+console.log(typeof newLogIn );
+console.log(newLogIn);
+
+
+
+/*   ------------ operation s -------------*/
+
+//there are some of the basic operation which can be performed //
+
+let ans = 2+2
+console.log(ans);
+
+
+let myName = "aahad"
+let yourName = myName
+
+console.log(yourName);
+yourName =  "falak"
+
+console.log(yourName)
+console.log(myName);
