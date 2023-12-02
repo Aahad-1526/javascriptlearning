2e0a56795e7a392dd3cce9e99d64c22d054db50a
Aahad-1526 <128283478+Aahad-1526@users.noreply.github.com>
2023-12-02 06:35:17 +0000

    lecture no 3

---
+++ basic/datatypes.js
@@ -0,0 +1,19 @@
+/*  in this file we will be learning about the various data type present in JS*/
+
+let Name= "aahad"
+let phoneNo = 6230252301
+let varified = true
+let gender
+let address = null
+
+//symbol is also a data type then object//
+console.log(typeof Name);
+console.log(typeof phoneNo)
+
+console.log(typeof undefined)
+console.log(typeof null);
+console.log( typeof address);
+/* the important point here to note is that typeof null is an OBJECT */
+//where as the typeof undefined is undefined//
+
+
---
+++ basic/variable.js
@@ -0,0 +1,25 @@
+const accountId= 1234
+let accountPassword= "hello"
+var accountEmail = "aahad@gmail.com"
+nameSur="hello"
+
+//accountId= 6789
+console.log(
+accountId
+);
+
+accountPassword = "bye"
+accountEmail = "falak@gmail.com"
+nameSur = "tata"
+
+//constantdatatype can not b changed any further
+
+console.table([accountEmail,accountPassword,accountId,nameSur])
+/* in javascript we do not use var because it have the problem with  the scope , because earlier 
+n js when a programmer use to change the value of any variable declared it will be changed everywhere
+which is quite a task to handle, instead we use let because it is good with the scope handling and always use let */
+
+"use strict" // read my code in the newer version 
+console.log(3+3)
+console.log(5+5)
+
